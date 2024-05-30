6889f94d5eede503aef93b20aa5fc3c9509fd8c9
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-05-29 22:34:41 -0400

    Add Memory Channel Split Enum

---
+++ src/memory_channel_split.h
@@ -0,0 +1,11 @@
+#ifndef MEMORY_CHANNEL_SPLIT_H
+#define MEMORY_CHANNEL_SPLIT_H
+
+enum class MemoryChannelSplit
+{
+    // TS140S/680S/711A/711E/811A/811B/811E only
+    RECEIVE,
+    TRANSMIT
+};
+
+#endif
