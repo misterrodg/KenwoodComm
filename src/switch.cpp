dacd4294765263eba6d020cb4160bdc8b4fe2502
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-06-07 13:41:40 -0400

    Switch to Class

---
+++ src/switch.cpp
@@ -0,0 +1,31 @@
+#include "switch.h"
+
+const int Switch::MAX_SWITCH_LENGTH = 3;
+
+Switch::SwitchEnum Switch::StringToSwitch(const std::string &switchString)
+{
+    static const std::map<std::string, Switch::SwitchEnum> enumMap = {
+        {"OFF", Switch::SwitchEnum::OFF},
+        {"ON", Switch::SwitchEnum::ON}};
+
+    auto it = enumMap.find(switchString);
+    if (it != enumMap.end())
+    {
+        return it->second;
+    }
+    return SwitchEnum::UNKNOWN;
+}
+
+std::string Switch::SwitchToString(const SwitchEnum &switchEnum)
+{
+    static const std::map<Switch::SwitchEnum, std::string> enumMap = {
+        {Switch::SwitchEnum::OFF, "OFF"},
+        {Switch::SwitchEnum::ON, "ON"}};
+
+    auto it = enumMap.find(switchEnum);
+    if (it != enumMap.end())
+    {
+        return it->second;
+    }
+    return "UNKNOWN";
+}
--- src/switch.h
+++ src/switch.h
@@ -1,10 +1,21 @@
 #ifndef SWITCH_H
 #define SWITCH_H
 
-enum class Switch
+#include <map>
+#include <string>
+
+class Switch
 {
-    OFF,
-    ON
+public:
+    enum class SwitchEnum
+    {
+        OFF,
+        ON,
+        UNKNOWN
+    };
+    static SwitchEnum StringToSwitch(const std::string &switchString);
+    static std::string SwitchToString(const SwitchEnum &switchEnum);
+    static const int MAX_SWITCH_LENGTH;
 };
 
 #endif
