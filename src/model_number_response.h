8f3d8bb5d1ef7cf6ce2e0dfaf91da5419d02100d
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-06-08 13:17:38 -0400

    Add Model Number Response

---
+++ src/model_number_response.h
@@ -0,0 +1,13 @@
+#ifndef MODEL_NUMBER_RESPONSE_H
+#define MODEL_NUMBER_RESPONSE_H
+
+enum class ModelNumberResponse
+// This quirk defined on page 24 of the IF-10A/B/C Manual
+{
+    TS711 = 1,
+    TS811 = 2,
+    TS940 = 3,
+    TS140TS680 = 6,
+};
+
+#endif
