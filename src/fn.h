2d7268093a887f50954f6822adad3d691429e5d2
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-06-08 11:37:02 -0400

    Add FN

---
+++ src/fn.h
@@ -0,0 +1,24 @@
+#ifndef FN_H
+#define FN_H
+
+#include "command.h"
+#include "command_prefix.h"
+#include "function.h"
+#include "model_number.h"
+#include <string>
+
+class Fn
+{
+public:
+    Fn(ModelNumber &modelNumberEnum);
+    bool SetFn(std::string &function);
+    std::string ToCommand();
+
+private:
+    bool fnAllowedForModelNumber(Function::FunctionEnum &functionEnum);
+    ModelNumber modelNumber;
+    Function::FunctionEnum function;
+    static const int FN_PARAMETER_LENGTH;
+};
+
+#endif
