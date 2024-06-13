e5d74717c7b247b333bfce38e992bb35ffc427d1
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2024-06-13 08:45:58 -0400

    Refine RIT Frequency

--- src/parameter/rit_frequency.cpp
+++ src/parameter/rit_frequency.cpp
@@ -2,21 +2,41 @@
 
 RITFrequency::RITFrequency() : frequencyInHz(0) {}
 
-void RITFrequency::setRITFrequency(const std::string &input)
+bool RITFrequency::setRITFrequency(const std::string &input)
 {
-    std::regex regex(R"(([+-]?\d{1,4}))");
+    bool result = false;
+    std::regex regex(R"(([+-]?\d+(\.\d+)?)([a-zA-Z]+))");
     std::smatch match;
 
-    if (std::regex_match(input, regex))
+    if (std::regex_match(input, match, regex))
     {
-        int value = std::stoi(input);
+        std::string sign = match[1].str();
+        double value = std::stod(match[2].str());
+        std::string unit = match[4].str();
 
-        frequencyInHz = value;
+        FrequencyUnit::Unit frequencyUnit = FrequencyUnit::parseUnit(unit);
+        if (frequencyUnit != FrequencyUnit::UNKNOWN && frequencyUnit != FrequencyUnit::MHZ && frequencyUnit != FrequencyUnit::GHZ)
+        {
+            frequencyInHz = FrequencyUnit::toHz(value, frequencyUnit);
+            if (frequencyInHz <= MAX_RIT_FREQUENCY_VALUE && frequencyInHz >= MIN_RIT_FREQUENCY_VALUE)
+            {
+                result = true;
+            }
+            else
+            {
+                printf("RIT frequency must be between %dHz and %dHz.\n", MAX_RIT_FREQUENCY_VALUE, MIN_RIT_FREQUENCY_VALUE);
+            }
+        }
+        else
+        {
+            printf("RIT frequency unit must be kHz or Hz.\n");
+        }
     }
     else
     {
-        printf("Invalid frequency format");
+        printf("Invalid RIT frequency format.\n");
     }
+    return result;
 }
 
 short int RITFrequency::getRITFrequencyInHz() const
--- src/parameter/rit_frequency.h
+++ src/parameter/rit_frequency.h
@@ -1,6 +1,7 @@
-#ifndef RITFREQUENCY_H
-#define RITFREQUENCY_H
+#ifndef RIT_FREQUENCY_H
+#define RIT_FREQUENCY_H
 
+#include "frequency_unit.h"
 #include <cstdint>
 #include <regex>
 #include <string>
@@ -9,9 +10,12 @@ class RITFrequency
 {
 public:
     RITFrequency();
-    void setRITFrequency(const std::string &input);
+    bool setRITFrequency(const std::string &input);
     short int getRITFrequencyInHz() const;
     std::string getRITFrequencyString();
+    static const int MAX_RIT_FREQUENCY_LENGTH = 5;
+    static const int MAX_RIT_FREQUENCY_VALUE = 9999;
+    static const int MIN_RIT_FREQUENCY_VALUE = -9999;
 
 private:
     short int frequencyInHz;
